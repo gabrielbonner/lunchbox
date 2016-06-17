6cac167176f23b012248cac3cceb827ddc22a318
Gabriel Bonner <gabrielbonner@gmail.com>
2016-06-17 16:00:41 -0700

    Attempt to refactor

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $.ajax({
+    method: 'GET',
+    dataType: 'json',
+    url: '/markers'
+  })
+    .done(function(response){
+      console.log(response);
+      for (var i = 0; i < response.length; i++) {
+        var marker_json = response[i];
+        console.log(marker_json.latitude);
+        console.log(marker_json.longitude);
+        var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(parseInt(marker_json.latitude), parseInt(marker_json.longitude)),
+          map: map
+        });
+      }
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    })
+    .fail(function(response){
+      console.log(response);
+    });
 });
